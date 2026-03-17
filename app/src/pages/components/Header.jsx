079323fe82118ebde8ea668ee90153899b6f8b78
Hassan Ali Tahir <hassanalitahir71@gmail.com>
2026-03-17 07:58:21 +0500

    Implement responsive Header and HeroSection

--- app/src/pages/components/Header.jsx
+++ app/src/pages/components/Header.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
+import { HiOutlineMenu, HiOutlineX } from "react-icons/hi";
 
 const Header = () => {
-  return (
-    <div className=" flex h-14  w-full mx-auto items-center  pl-21 pr-55  justify-between">
-      <img src="images/logo.png" alt=""  className='h-full'/>
-      <nav className="flex text-White1  text-lg gap-12 font-lexend font-normal" >
-        <a href="" className="">
-          HOME
-        </a>
-        <a href="" className="">
-          SERVICES
-        </a>
-        <a href="" className="">
-          FEATURES
-        </a>
-        <a href="" className="">
-          PORTFOLIO
-        </a>
-        <a href="" className="">
-          BLOG
-        </a>
-        <a href="" className="">
-          CONTACT
-        </a>
-      </nav>
-    </div>
-  );
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Header
+  return (
+    <header className="flex items-center justify-between h-14 minw-full px-6 md:px-45 ">
+      {/* Logo */}
+      <img src="images/logo.png" alt="Logo" className="h-full" />
+
+      {/* Desktop Nav */}
+      <nav className="hidden md:flex text-White1 text-lg gap-8 mr-15 font-lexend font-normal">
+        <a href="#">HOME</a>
+        <a href="#">SERVICES</a>
+        <a href="#">FEATURES</a>
+        <a href="#">PORTFOLIO</a>
+        <a href="#">BLOG</a>
+        <a href="#">CONTACT</a>
+      </nav>
+
+      {/* Mobile Menu Button */}
+      <button
+        className="md:hidden text-White1 text-2xl"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {isOpen ? <HiOutlineX /> : <HiOutlineMenu />}
+      </button>
+
+      {/* Mobile Nav */}
+      {isOpen && (
+        <nav className="absolute top-14 left-0 w-full bg-white flex flex-col items-center py-4 space-y-4 md:hidden">
+          <a href="#">HOME</a>
+          <a href="#">SERVICES</a>
+          <a href="#">FEATURES</a>
+          <a href="#">PORTFOLIO</a>
+          <a href="#">BLOG</a>
+          <a href="#">CONTACT</a>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
