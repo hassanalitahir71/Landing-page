9e485f9b7b5b9b859abbd83de0e891d297e95d39
Hassan Ali Tahir <hassanalitahir71@gmail.com>
2026-03-28 21:03:21 +0500

    Update ClientAbout component: add grid layout and UI improvements

--- app/src/pages/components/ClientAbout.jsx
+++ app/src/pages/components/ClientAbout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Slider from "react-slick";
+import { LuArrowDownRight } from "react-icons/lu";
 
 const ClientAbout = () => {
   const LogoList = [
@@ -97,8 +98,8 @@ const ClientAbout = () => {
     slidesToShow: 4,
     slidesToScroll: 5,
     autoplay: true,
-    speed: 2000,
-    autoplaySpeed: 2000,
+    speed: 3000,
+    autoplaySpeed: 3000,
     cssEase: "linear",
     pauseOnHover: false,
     responsive: [
@@ -200,6 +201,76 @@ const ClientAbout = () => {
                 </div>
               ))}
             </Slider>
+          </div>
+        </div>
+        {/* Creative Team */}
+        <div
+          className=" min-h-screen flex items-center justify-center mt-10 
+         p-6"
+        >
+          <div className="grid grid-cols-3 auto-rows-[170px] gap-5 max-w-6xl w-full">
+            {/* TEXT */}
+            <div className="col-span-2 font-lexend  ">
+              <h1 className=" text-White1 text-5xl font-bold "> THE MINDS</h1>
+              <h2 className=" text-5xl font-bold text-Brown1">BEHIND</h2>
+
+              <div className=" flex flex-row gap-8 items-center">
+                <p className=" text-3xl text-gray-300">
+                  OUR <span className="text-Brown1  font-bold">CREATIVE</span>{" "}
+                  TEAM
+                </p>
+                <button className=" flex flex-row items-center gap-1 hover:bg-Orange1 bg-Brown1 px-3 py-2 rounded-full text-White1">
+                  Explore More
+                  <LuArrowDownRight />
+                </button>
+              </div>
+            </div>
+
+            {/* TOP RIGHT IMAGE */}
+            <div className="row-span-1 flex justify-end overflow-hidden p-1">
+              <img
+                src="/images/grid_img2.jpg"
+                alt="
+            "
+                className="w-50 h-full object-cover rounded-tr-4xl  rounded-bl-4xl  rounded-tl-4xl"
+              />
+            </div>
+
+            {/* LEFT BIG IMAGE */}
+            <div className="row-span-2 rounded-4xl overflow-hidden">
+              <img
+                src="/images/Largeimg.jpg"
+                alt=""
+                className="w-full h-full rounded-4xl object-cover"
+              />
+            </div>
+
+            {/* CENTER CIRCLE IMAGE */}
+            <div className="flex items-center justify-center row-span-1">
+              <img
+                src="/images/grid_img3.jpg"
+                alt=""
+                className="w-50 h-50 rounded-full object-cover"
+              />
+            </div>
+
+            {/* RIGHT SMALL IMAGE */}
+            <div className=" overflow-hidden pt-2 flex justify-end">
+              <img
+                src="/images/grid_img4.jpg"
+                alt=""
+                className="w-40 h-40  rounded-tr-4xl  rounded-bl-4xl  "
+              />
+            </div>
+
+            {/* BOTTOM WIDE IMAGE */}
+            <div className="col-span-2  flex justify-center items-center overflow-hidden">
+              <img
+                src="/images/grid_img5.jpg"
+                alt=""
+                className="w-[60%] h-[70%] rounded-4xl object-cover"
+              />
+            </div>
           </div>
         </div>
       </div>
